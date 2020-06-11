225642ab1539480cfc2a57e6e8a70def995a50ab
Murilo Oliari Ribeiro <murilo.oliari@gmail.com>
2020-06-11 00:52:37 -0300

    alteraçoes no código

--- src/components/FeedPage/index.js
+++ src/components/FeedPage/index.js
@@ -4,18 +4,54 @@ import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 import styled from 'styled-components';
 import useForm from '../Hooks/useForm';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowAltCircleUp } from '@fortawesome/free-solid-svg-icons';
+import { faArrowAltCircleDown } from '@fortawesome/free-solid-svg-icons';
+
 
 import NavigationBar from '../NavigationBar/NavigationBar'
 
 const Wrapper = styled.section`
+    overflow: auto;
+    background-color: #d9d9d9;
+`
+
+const Blau = styled.div`
+    position: absolute;
+    top: 90px;
+    left: 0px;
+    bottom: 10px;
+    right: 16px;
+    padding: 30px; 
+    overflow-y: scroll;
+    overflow-x: hidden;
+
+    ::-webkit-scrollbar-thumb {
+        -webkit-border-radius: 10px;
+        border-radius: 10px;
+        background: orange; 
+        -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.5); 
+    }
+
+    ::-webkit-scrollbar {
+        width: 12px;
+    }
+
+    ::-webkit-scrollbar-track {
+        -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3); 
+        -webkit-border-radius: 10px;
+        border-radius: 10px;
+    }
+    
+    ::-webkit-scrollbar-thumb:window-inactive {
+        background: rgba(255,0,0,0.4); 
+    }
 
     display: grid;
     grid-template-areas: 'navi navi navi' 
                          'a b c';
-    grid-template-areas: 8% 92%;
+    grid-template-rows: 8% 92%;
     grid-template-columns: 1fr 2fr 1fr;
-    background-color: #d9d9d9;
-
 `
 
 const A = styled.div`
@@ -31,10 +67,50 @@ const C = styled.div`
 `
 
 const Post = styled.div`
+    max-height: 250px;
+    display: grid;
+    grid-template-areas: 'side user'
+                         'side title' 
+                         'side container'
+                         'side comments';
+    grid-template-rows: 20% 20% 40% 20%;
+    grid-template-columns: 1fr 9fr;
+
+
     border: 1px solid black;
     border-radius: 5px;
     margin: 2.5%;
     background-color: whitesmoke;
+`
+
+const VoteBar = styled.div`
+
+    display: flex;
+    grid-area: side;
+    padding-top: 20%;
+    /**border: 1px solid red;*/
+    align-items: center;
+    flex-direction: column;
+`
+
+const User = styled.div`
+    grid-area: user;
+    /**border: 1px solid yellow;*/
+`
+
+const Title = styled.div`
+    grid-area: title;
+    /**border: 1px solid green;*/
+`
+
+const Content = styled.div`
+    grid-area: container;
+    /**border: 1px solid blue;*/
+`
+
+const Comments = styled.div`
+    grid-area: comments;
+    /**border: 1px solid purple;*/
 `
 
 const InputsForm = styled.div`
@@ -120,6 +196,7 @@ const FeedPage = () => {
     return (
         <Wrapper>
             <NavigationBar />
+            <Blau>
             <A />
             <B>
                 <InputsForm>
@@ -139,24 +216,45 @@ const FeedPage = () => {
                         onChange={handleInputChange}
                         required
                     />
-                    <PrimaryButton onClick={createPost}>Ciar</PrimaryButton>
+                    <PrimaryButton onClick={createPost}>Criar</PrimaryButton>
                 </InputsForm>
                 {post && post.map(post => {
                     return (
                         <Post key={post.id}>
-                            <div onClick={() => goToPostComments(post.id)}>
-                                <h6>4/{post.username}</h6>
-                                <h4>{post.title}</h4>
-                                <p>{post.text}</p>
-                                <p>{post.voteCount} {post.userVoteDirection} | 💬 {post.commentsCount}</p>
-                            </div>
-                            <button onClick={() => vote(post.id, +1)}>🔺</button>
-                            <button onClick={() => vote(post.id, -1)}>🔻</button>
+                                <User>
+                                    <h6>4/{post.username}</h6>
+                                </User>
+                                <Title>
+                                    <h4>{post.title}</h4>
+                                </Title>
+                                <Content onClick={() => goToPostComments(post.id)}>
+                                    <p>{post.text}</p>
+                                </Content>
+                                <Comments>
+                                    <p>💬 {post.commentsCount} {post.commentsCount > 1? 'Comments' : 'Comment' }</p>
+                                </Comments>
+                                <VoteBar>
+                                    <FontAwesomeIcon
+                                        icon={faArrowAltCircleUp}
+                                        size='lg'
+                                        color='gray'
+                                        onClick={() => vote(post.id, +1)}
+                                    />
+                                    {/**<p>{post.voteCount}</p>*/}
+                                    <p>{post.userVoteDirection}</p>
+                                    <FontAwesomeIcon
+                                        icon={faArrowAltCircleDown}
+                                        size='lg'
+                                        color='gray'
+                                        onClick={() => vote(post.id, -1)}
+                                    />
+                                </VoteBar>
                         </Post>
                     )
                 })}
             </B>
             <C />
+            </Blau>
         </Wrapper>
     );
 };
